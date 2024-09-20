0752aa750f54a5568c3679a53b1d0e2c7be48b31
Tim Nunamaker <tim.nunamaker@gmail.com>
2024-09-19 19:56:26 -0500

    Config check refactor

--- container-scripts/check-config.sh
+++ container-scripts/check-config.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# Check if WITHDRAWAL_ADDRESS is set and not the default value
-if [ -z "$WITHDRAWAL_ADDRESS" ] || [ "$WITHDRAWAL_ADDRESS" = "0x0000000000000000000000000000000000000000" ]; then
-    echo "Error: WITHDRAWAL_ADDRESS is not set or is still the default value."
-    echo "Please set a valid withdrawal address in your .env file."
-    exit 1
-fi
-
-# Check if NETWORK is empty
-if [ -z "$NETWORK" ]; then
-  echo "Error: NETWORK is not set. Please set the NETWORK environment variable to either 'moksha' or 'mainnet'"
-  exit 1
-fi
-
 # Check NETWORK
 if [ "$NETWORK" != "moksha" ] && [ "$NETWORK" != "mainnet" ]; then
   echo "Error: Invalid NETWORK '$NETWORK', must be either 'moksha' or 'mainnet'"
@@ -29,9 +16,24 @@ fi
 
 echo "Network configuration files check passed"
 
+# Check if JWT secret exists
+if [ ! -f /vana/data/jwt.hex ]; then
+  echo "Error: JWT secret not found at /vana/data/jwt.hex. See README.md for instructions on how to generate a JWT secret."
+  exit 1
+fi
+
+echo "JWT secret check passed"
 # Check if we need to perform validator-related checks
 if [ "$USE_VALIDATOR" = "true" ]; then
   echo "Performing validator-specific checks..."
+
+  # Check if WITHDRAWAL_ADDRESS is set and not the default value
+  if [ -z "$WITHDRAWAL_ADDRESS" ] || [ "$WITHDRAWAL_ADDRESS" = "0x0000000000000000000000000000000000000000" ]; then
+      echo "Error: WITHDRAWAL_ADDRESS is not set or is still the default value."
+      echo "Please set a valid withdrawal address in your .env file."
+      exit 1
+  fi
+
 
   # Check if validator keys exist and have been imported
   if [ ! -d /vana/secrets ] || [ -z "$(ls -A /vana/secrets)" ]; then
@@ -69,59 +71,51 @@ if [ "$USE_VALIDATOR" = "true" ]; then
   fi
 
   echo "DEPOSIT_PRIVATE_KEY check passed"
+
+  # Check if account password file exists
+  if [ ! -f /vana/secrets/account_password.txt ]; then
+    echo "Error: Account password file not found at /vana/secrets/account_password.txt. See README.md for instructions on how to set up validator keys and password."
+    exit 1
+  fi
+
+  echo "Account password file check passed"
+
+  # Check if wallet password file exists
+  if [ ! -f /vana/secrets/wallet_password.txt ]; then
+    echo "Error: Wallet password file not found at /vana/secrets/wallet_password.txt. See README.md for instructions on how to set up validator keys and password."
+    exit 1
+  fi
+
+  echo "Wallet password file check passed"
+
+  # Check DEPOSIT_PRIVATE_KEY
+  if [ -z "$DEPOSIT_PRIVATE_KEY" ] || [ "$DEPOSIT_PRIVATE_KEY" = "0000000000000000000000000000000000000000000000000000000000000000" ]; then
+      echo "Error: DEPOSIT_PRIVATE_KEY is not set or is still the default value."
+      echo "Please set a valid private key for deposits in your .env file."
+      exit 1
+  fi
+
+  echo "DEPOSIT_PRIVATE_KEY check passed"
+
+  # Check DEPOSIT_RPC_URL
+  if [ -z "$DEPOSIT_RPC_URL" ]; then
+      echo "Error: DEPOSIT_RPC_URL is not set."
+      echo "Please set a valid RPC URL for deposits in your .env file."
+      exit 1
+  fi
+
+  echo "DEPOSIT_RPC_URL check passed"
+
+  # Check DEPOSIT_CONTRACT_ADDRESS
+  if [ -z "$DEPOSIT_CONTRACT_ADDRESS" ]; then
+      echo "Error: DEPOSIT_CONTRACT_ADDRESS is not set."
+      echo "Please set a valid deposit contract address in your .env file."
+      exit 1
+  fi
+
+  echo "DEPOSIT_CONTRACT_ADDRESS check passed"
 else
   echo "Skipping validator-specific checks..."
 fi
-
-# Check if account password file exists
-if [ ! -f /vana/secrets/account_password.txt ]; then
-  echo "Error: Account password file not found at /vana/secrets/account_password.txt. See README.md for instructions on how to set up validator keys and password."
-  exit 1
-fi
-
-echo "Account password file check passed"
-
-# Check if wallet password file exists
-if [ ! -f /vana/secrets/wallet_password.txt ]; then
-  echo "Error: Wallet password file not found at /vana/secrets/wallet_password.txt. See README.md for instructions on how to set up validator keys and password."
-  exit 1
-fi
-
-echo "Wallet password file check passed"
-
-# Check DEPOSIT_PRIVATE_KEY
-if [ -z "$DEPOSIT_PRIVATE_KEY" ] || [ "$DEPOSIT_PRIVATE_KEY" = "0000000000000000000000000000000000000000000000000000000000000000" ]; then
-    echo "Error: DEPOSIT_PRIVATE_KEY is not set or is still the default value."
-    echo "Please set a valid private key for deposits in your .env file."
-    exit 1
-fi
-
-echo "DEPOSIT_PRIVATE_KEY check passed"
-
-# Check DEPOSIT_RPC_URL
-if [ -z "$DEPOSIT_RPC_URL" ]; then
-    echo "Error: DEPOSIT_RPC_URL is not set."
-    echo "Please set a valid RPC URL for deposits in your .env file."
-    exit 1
-fi
-
-echo "DEPOSIT_RPC_URL check passed"
-
-# Check DEPOSIT_CONTRACT_ADDRESS
-if [ -z "$DEPOSIT_CONTRACT_ADDRESS" ]; then
-    echo "Error: DEPOSIT_CONTRACT_ADDRESS is not set."
-    echo "Please set a valid deposit contract address in your .env file."
-    exit 1
-fi
-
-echo "DEPOSIT_CONTRACT_ADDRESS check passed"
-
-# Check if JWT secret exists
-if [ ! -f /vana/data/jwt.hex ]; then
-  echo "Error: JWT secret not found at /vana/data/jwt.hex. See README.md for instructions on how to generate a JWT secret."
-  exit 1
-fi
-
-echo "JWT secret check passed"
 
 echo "All configuration checks passed"
